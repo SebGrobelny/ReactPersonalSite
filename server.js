ad7788f3378a9ff1eefd7d6cd41b933c98f2b146
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2017-09-12 00:42:10 -0700

    deployment to heroku

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ app.use(webpackDevMiddleware(compiler, {
   },
   historyApiFallback: true,
 }));
- 
-const server = app.listen(3000, function() {
+var port = process.env.PORT || 8080;
+
+const server = app.listen(port, function() {
   const host = server.address().address;
   const port = server.address().port;
+
   console.log('Example app listening at http://%s:%s', host, port);
 });
